f287e0d2bba2399856f10792b6a769061ae09153
Cristhian <Cristhian@CristhianBarrios>
2018-03-11 01:17:08 -0600

    Primer commit

---
+++ src/ipc1/tarea4_201610703/IPC1Tarea4_201610703.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.tarea4_201610703;
+
+/**
+ *
+ * @author Cristhian
+ */
+public class IPC1Tarea4_201610703 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
